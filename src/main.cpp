0ac03c9d1476ed90b6ec437b96e1fa5301c2b2fb
Mitch <97131414+Mitchfox1@users.noreply.github.com>
2024-11-15 22:51:48 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Flight Computer Initializing...");
+}
+
+void loop() {
+    // Main flight computer code will go here
+}
